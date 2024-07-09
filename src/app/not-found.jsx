9e19fed7247a8e539c40ef6dc86f0afcc7a98bce
alexrem1 <remidy@live.co.uk>
2024-07-09 15:26:14 +0100

    not found

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -1,10 +1,11 @@
 import Link from "next/link";
+import styles from "./not-found.module.css";
 
 function NotFound() {
   return (
-    <div>
+    <div className={styles.container}>
       <h2>Not Found</h2>
-      <p>Sorry, the page you&apos;re looking for does not exist</p>
+      <p>Sorry, the page you&apos;re looking for does not exist.</p>
       <Link href="/">Back to Homepage</Link>
     </div>
   );
